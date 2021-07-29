223281c6f19426588e0a7f5970582c40976580ac
David R. Walton <drwalton@users.noreply.github.com>
2021-07-29 15:02:57 +0100

    Add example simple compositing shader

---
+++ UnityProject/Assets/CompositeImage.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CompositeImage : MonoBehaviour
+{
+    #region shaders
+    private Shader m_shaderOverlayImages;
+    public Shader shaderOverlayImages
+    {
+        get
+        {
+            if (m_shaderOverlayImages == null)
+                m_shaderOverlayImages = Shader.Find("Hidden/OverlayImages");
+
+
+            return m_shaderOverlayImages;
+        }
+    }
+
+    private Material m_MaterialOverlayImages;
+    public Material materialOverlayImages
+    {
+        get
+        {
+            if (m_MaterialOverlayImages == null)
+            {
+                if (shaderOverlayImages == null || shaderOverlayImages.isSupported == false)
+                    return null;
+
+                m_MaterialOverlayImages = new Material(shaderOverlayImages);
+            }
+
+            return m_MaterialOverlayImages;
+        }
+    }
+    #endregion
+
+    public Camera handCamera;
+    public VertClient vertClient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        materialOverlayImages.SetTexture("_VirtualTex", source);
+        var handTex = RenderTexture.GetTemporary(
+            handCamera.pixelWidth, handCamera.pixelHeight, 24, 
+            RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+        handCamera.targetTexture = handTex;
+        handCamera.Render();
+        materialOverlayImages.SetTexture("_HandsTex", handTex);
+
+        Graphics.Blit(vertClient.receiveTexture, destination, materialOverlayImages);
+    }
+}
---
+++ UnityProject/Assets/Resources/Shaders/OverlayImages.shader
@@ -0,0 +1,59 @@
+﻿Shader "Hidden/OverlayImages"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+	}
+		CGINCLUDE
+#include "UnityCG.cginc"
+
+	sampler2D _MainTex;
+	sampler2D _VirtualTex;
+	sampler2D _HandsTex;
+
+	struct Input
+	{
+		float4 vertex : POSITION;
+		float2 uv : TEXCOORD0;
+	};
+
+	struct Varyings
+	{
+		float4 vertex : SV_POSITION;
+		float2 uv : TEXCOORD0;
+	};
+
+	Varyings vertex(in Input input)
+	{
+		Varyings output;
+		output.vertex = UnityObjectToClipPos(input.vertex.xyz);
+		output.uv = input.uv;
+
+		return output;
+	}
+
+	float4 fragment(in Varyings input) : SV_Target
+	{
+		float4 imageColor = tex2D(_MainTex, input.uv);
+		float4 virtualColor = tex2D(_VirtualTex, input.uv);
+		float4 handsColor = tex2D(_HandsTex, input.uv);
+
+		float4 outputColor = (imageColor + virtualColor + handsColor) / 3.0;
+
+		return outputColor;
+	}
+		ENDCG
+
+		SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+			Pass
+		{
+			CGPROGRAM
+			#pragma vertex vertex
+			#pragma fragment fragment
+			ENDCG
+		}
+	}
+}
